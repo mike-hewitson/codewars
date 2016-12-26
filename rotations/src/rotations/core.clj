6584d05dd0fd1d3e1b69300721f412e5edff0376
Mike Hewitson <mike@hewitson.co.za>
2016-12-26 14:44:27 +0200

    feat; complete include all

--- rotations/src/rotations/core.clj
+++ rotations/src/rotations/core.clj
@@ -1,5 +1,19 @@
 (ns rotations.core)
 
-(defn contain-all-rots [strng arr])
-  ; your code
-  
+(defn build-rotations
+  ([strng]
+   (build-rotations strng [] (.length strng)))
+  ([strng rotations rotations-to-go]
+   (let [rotated (str (subs strng 1) (subs strng 0 1))]
+     (if (= rotations-to-go 0)
+       rotations
+       (recur rotated (conj rotations rotated) (dec rotations-to-go))))))
+
+(defn result-compare [rotations arr]
+  (cond
+    (empty? rotations) true
+    (some #(= (first rotations) %) arr) (result-compare (rest rotations) arr)
+    :else false))
+
+(defn contain-all-rots [strng arr]
+  (every? (set arr) (build-rotations strng)))
