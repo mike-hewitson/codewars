49ed19eec0fada23ceab766eb8cc59b06bfe0fa4
Mike Hewitson <mike@hewitson.co.za>
2017-01-24 15:51:07 +0200

    chore: work on sum of cubes

--- sumofcubes/spec/sumofcubes/core_spec.clj
+++ sumofcubes/spec/sumofcubes/core_spec.clj
@@ -3,8 +3,13 @@
             [sumofcubes.core :refer :all]))
 
 (describe "sum of cubes"
-  (it "should give a correct answer"
-    (should= (find-nb 4183059834009) 2022)
-    (should= (find-nb 24723578342962) -1)
-    (should= (find-nb 135440716410000) 4824)
+  (it "should give a correct answer given 100"
+    (should= 4 (find-nb 100)))
+  (it "should give a correct answer given 4183059834009"
+    (should= (find-nb 4183059834009) 2022))
+  (it "should give a correct answer given 24723578342962"
+    (should= (find-nb 24723578342962) -1))
+  (it "should give a correct answer given 135440716410000"
+    (should= (find-nb 135440716410000) 4824))
+  (it "should give a correct answer given 40539911473216"
     (should= (find-nb 40539911473216) 3568)))
--- sumofcubes/src/sumofcubes/core.clj
+++ sumofcubes/src/sumofcubes/core.clj
@@ -1,13 +1,49 @@
-(ns sumofcubes.core)
+(ns sumofcubes.core
+  (:require [proto-repl.saved-values :refer :all]))
+
+; this solution takes too long, need a mathematical one
+; (defn find-nb
+;   ([m]
+;    (find-nb m 1))
+;   ([m i]
+;    (cond
+;      (= m 0) (- i 1)
+;      (< m 0) -1
+;      :else (recur (- m (*' i i i))
+;                   (inc i)))))
+
+; (find-nb 102525193459636447345267623154781254785178)
+(Math/sqrt 102525193459636447345267623154781254785178)
+(defn sqr [n] (* n n))
+
+(defn sum-of-cubes
+  [n]
+  (/ (* (sqr n)
+        (sqr (+ n 1)))
+     4))
 
 (defn find-nb
-  ([m]
-   (find-nb m 1))
-  ([m i]
-   (cond
-     (= m 0) (- i 1)
-     (< m 0) -1
-     :else (recur (- m (*' i i i))
-                  (inc i)))))
+  [n]
+  (let [root (Math/sqrt n)]
+    (proto-repl.saved-values/save 1)
+    (if (= (Math/floor root) root)
+        (dec root)
+        -1)))
 
-(find-nb 102525193459636447345267623154781254785178)
+
+(find-nb 9)
+(sum-of-cubes 2)
+(Math/cbrt 225)
+(Math/sqrt 225)
+
+(defn n-given-tri
+  [tri]
+  (/ (- (Math/sqrt (+ 1
+                      (* 8 tri)))
+        1)
+     2))
+
+(n-given-tri 6)
+
+(defn n-th-tri
+  [n])
