77453aea8a631be3cce4b8eba10d4e7c43d4c9af
Mike Hewitson <mike@hewitson.co.za>
2017-01-26 17:02:48 +0200

    begin stats

--- stat/project.clj
+++ stat/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [proto-repl "0.3.1"]]
+                 ; [proto-repl-sayid "0.1.3"]]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
   :plugins [[speclj "3.3.1"]]
   :test-paths ["spec"])
--- stat/src/stat/core.clj
+++ stat/src/stat/core.clj
@@ -1,1 +1,13 @@
 (ns stat.core)
+
+(defn split-time
+  [time]
+  ((fn [[hh mm ss]]
+     (+ (* hh 3600) (* mm 60) ss))
+   (map read-string (clojure.string/split time #"\|"))))
+
+(defn join-time
+  [seconds]
+  (str (quot seconds 3600) "|"
+       (- (quot seconds 60) (* (quot seconds 3600) 60)) "|"
+       (mod seconds 60)))
