f57aa032ccea5243444de45070d2750fd86bb446
mikeyjcat <mike@hewitson.co.za>
2017-05-05 08:08:32 +0200

    completed stats

--- stat/project.clj
+++ stat/project.clj
@@ -1,5 +1,4 @@
 (defproject stat "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
--- stat/src/stat/core.clj
+++ stat/src/stat/core.clj
@@ -1,27 +1,49 @@
 (ns stat.core
   (:require [clojure.string :as str]))
 
-(defn split-time
+(defn time->int
   [time]
   ((fn [[hh mm ss]]
      (+ (* hh 3600) (* mm 60) ss))
    (map read-string (clojure.string/split time #"\|"))))
 
-(defn join-time
+(defn int->time
   [seconds]
   (str (format "%02d" (quot seconds 3600)) "|"
-       (- (quot seconds 60) (* (quot seconds 3600) 60)) "|"
-       (mod seconds 60)))
+       (format "%02d"(- (quot seconds 60) (* (quot seconds 3600) 60))) "|"
+       (format "%02d" (mod seconds 60))))
 
 (defn split-up-times
   [times]
   (str/split times #", "))
 
-(defn average
+(defn mean
   [coll]
-  (Math/round (float (/ (reduce + coll) (count coll)))))
+  (int (/ (reduce + coll) (count coll))))
+
+(defn my-range
+  [coll]
+  (- (apply max coll) (apply min coll)))
+
+(defn median [coll]
+  (let [sorted (sort coll)
+        cnt (count sorted)
+        halfway (quot cnt 2)]
+    (if (odd? cnt)
+      (nth sorted halfway)
+      (let [bottom (dec halfway)
+            bottom-val (nth sorted bottom)
+            top-val (nth sorted halfway)]
+        (mean [bottom-val top-val])))))
 
 (defn stat
   [times]
-  (str " Average: "
-       (join-time (average (map split-time (split-up-times times))))))
+  (let [split-times (map time->int (split-up-times times))]
+    (if (empty? times)
+      ""
+      (str "Range: "
+           (int->time (my-range split-times))
+           " Average: "
+           (int->time (mean split-times))
+           " Median: "
+           (int->time (median split-times))))))
