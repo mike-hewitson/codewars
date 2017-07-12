32e8b35d48799342490cfa3f1e4368dbb67d9b71
Mike Hewitson <mike@hewitson.co.za>
2017-07-12 16:35:46 +0200

    beginning the process

--- moduli/test/moduli/core_test.clj
+++ moduli/test/moduli/core_test.clj
@@ -5,4 +5,10 @@
 
 (facts "a-test1"
        (fact "1st Test"
-         (fromNb2Str 187 [8 7 5 3]) => "-3--5--2--1-"))
+         (fromNb2Str 187 [8 7 5 3]) => "-3--5--2--1-"
+         (fromNb2Str 11 [2 3 5]) => "-1--2--1-"
+         (fromNb2Str 6 [2 3 4]) => "Not applicable"
+                                        ;since 2 and 4 are not coprime
+         (fromNb2Str 7 [2 3]) => "Not applicable"
+                                        ;since 2 * 3 < 7
+         ))
