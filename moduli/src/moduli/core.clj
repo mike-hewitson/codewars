044482128528bd33bad0f29ff9f7ca257fb79977
Mike Hewitson <mike@hewitson.co.za>
2017-07-12 16:35:46 +0200

    feat: beginning the process

--- moduli/src/moduli/core.clj
+++ moduli/src/moduli/core.clj
@@ -1,7 +1,16 @@
 (ns moduli.core)
 
+(defn test-correctness
+  [n arr]
+  (cond
+    (< n (reduce * arr)) true))
+
 
 (defn fromNb2Str
   [n arr]
-  "-"
+  (if (test-correctness n arr)
+    "Not applicable"
+    "-")
   )
+
+
